13f6ee158e1c13ae33970bcb8f1ff356ef06a7ac
John Philip <johnphilipomamalin1206@gmail.com>
2025-03-04 08:22:05 +0800

    Create SpringBootJPhlpLApplicationSample.java

---
+++ server/src/main/java/com/spring/demo/SpringBootJPhlpLApplicationSample.java
@@ -0,0 +1,13 @@
+package com.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJPhlpLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJPhlpLApplication.class, args);
+	}
+
+}
